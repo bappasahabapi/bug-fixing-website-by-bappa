7227f37d0a5c8c26ec45cb02e6f9443252f64b68
bappasahabapi <bappasahabapi@gmail.com>
2021-09-13 03:34:26 +0600

    update the title

--- js/app.js
+++ js/app.js
@@ -22,7 +22,10 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h5>Rating: ${product.rating.rate}</h5>
+      <h5>Count: ${product.rating.count}</h5>
       <h2>Price: $ ${product.price}</h2>
+      
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
